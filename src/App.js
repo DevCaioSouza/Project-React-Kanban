cb946d0436e99c7c7d19fd0bc536777e8e369c99
caio souza <caiocbahia@hotmail.com>
2022-02-05 21:05:37 -0300

    Parte 15 da aula encerrada

--- src/App.js
+++ src/App.js
@@ -1,21 +1,52 @@
-import logo from './logo.svg';
+import React, { useState } from "react";
+// import logo from './logo.svg';
 import './App.css';
 
 import Navbar from "./components/Navbar/Navbar"
 import TaskList from "./components/TaskList/TaskList"
 
+const task = {
+  id: 0,
+  title: "Nova Tarefa",
+  state: "pendente"
+};
+
+let idAcc = 0;
+const generateId = () => {
+  idAcc = idAcc + 1;
+  return idAcc;
+}
+
 function App() {
+
+  const [tasks, setTasks] = useState([]);
+
+  const addTask = (title, state) => {
+    const newTask = {
+      id: generateId(),
+      title, 
+      state
+    }
+    setTasks((existingTasks) => {
+      return [...existingTasks, newTask];
+    })
+  }
+
   return (
     <div className="App">
       <Navbar /> {/* Assim uma função é chamada no React.
           Fizemos aqui uma "Composição de componentes React" */}
-      <div>
-      <TaskList title="Pendente"/>
-      <TaskList title="Fazendo"/>
-      <TaskList title="Completa"/>
+      <div className='container'>
+        <TaskList title="Pendente" onAddTask={addTask} tasks={tasks} />
       </div>
     </div>
   );
 }
 
 export default App;
+
+// palavras do professor:
+// "Como manipular estado em aplicações react:
+// Temos aqui no App.js o nosso container com todas as tarefas,
+// nós temos uma função pra adicionar tarefa ao clicar no botão
+// e temos o nosso componente TaskList encarregado de chamar essa função addTask"
