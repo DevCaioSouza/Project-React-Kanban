86b0fc67f16ba91458f792b9e1c3ac82cc2618c7
caio souza <caiocbahia@hotmail.com>
2022-02-06 19:22:35 -0300

    Aula 20 finalizada e PROJETO FINALIZADO!

--- src/components/TaskList/TaskList.js
+++ src/components/TaskList/TaskList.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./tasklist.css"
 import PropTypes from "prop-types";
+import plusIcon from '../../img/plus-icon.svg'
 // Para adicionar propTypes, digite no terminal "npm install prop-types"
 
 import TaskItem from "../TaskItem/TaskItem";
@@ -23,16 +24,24 @@ export default function TaskList({
             <div className="title">{title}</div>
             <div className="content">
                 {tasks.map((task) => {
-                    return <TaskItem 
+                  return (
+                    <TaskItem 
                     key={task.id}
                     id={task.id} 
                     title={task.title} 
                     taskState={task.state}
                     onTaskUpdate={onTaskUpdate}
-                    onDeleteTask={onDeleteTask} />
+                    onDeleteTask={onDeleteTask} 
+                    />
+                    )  
                 })}
-            </div>
-            <button onClick={addTask}>Adicionar Tarefa</button>
+                { tasks.length === 0 && 
+                <div className="empty-list">Lista Vazia</div> }
+                <button onClick={addTask} className="btn">
+                <img src={plusIcon} alt="plus"/>
+                Adicionar Tarefa
+            </button>
+            </div>   
         </div>
     )
 }
